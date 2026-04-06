49b2cd6673509d016e5a854de2e4731a4b012f5e
Rizki Ramadhan <rizkir20122000@gmail.com>
2026-04-06 14:17:13 +0700

    fixed:bugs for production

--- fe/astro.config.mjs
+++ fe/astro.config.mjs
@@ -2,9 +2,20 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        plugins: [tailwindcss()],
-      },
+  session: {
+    driver: {
+      entrypoint: "unstorage/drivers/null",
+    },
+  },
+  vite: {
+      plugins: [tailwindcss()],
+    },
+
+  adapter: cloudflare({
+    imageService: "compile",
+  }),
 });
